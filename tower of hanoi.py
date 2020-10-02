a1e2a9a5bd6a2c9165ad4a3fc38713732e8004f7
B C Sagar <62722608+99bcsagar@users.noreply.github.com>
2020-10-02 12:04:37 +0530

    Tower of Hanoi
    its one of the imporant  questions asked in interviews

---
+++ tower of hanoi.py
@@ -0,0 +1,21 @@
+#a mathematical puzzle where we have three rods and n disks. The objective of the puzzle is to move the entire stack to another rod, obeying the following simple rules:
+#1) Only one disk can be moved at a time.
+#2) Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack i.e. a disk can only be moved if it is the uppermost disk on a stack.
+#3) No disk may be placed on top of a smaller disk.
+
+# Recursive Python function 
+
+def TowerOfHanoi(n , from_rod, to_rod, aux_rod): 
+	if n == 1: 
+		print "Move disk 1 from rod",from_rod,"to rod",to_rod 
+		return
+	TowerOfHanoi(n-1, from_rod, aux_rod, to_rod) 
+	print "Move disk",n,"from rod",from_rod,"to rod",to_rod 
+	TowerOfHanoi(n-1, aux_rod, to_rod, from_rod) 
+		
+# Driver code 
+n = 4
+TowerOfHanoi(n, 'A', 'C', 'B') 
+# A, C, B are rods 
+
+
