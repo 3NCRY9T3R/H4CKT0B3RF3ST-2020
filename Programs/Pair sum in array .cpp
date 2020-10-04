06e534dc5512be23f96209e802f179bfc31f0ffe
Ashutosh2000-cyber <56223503+Ashutosh2000-cyber@users.noreply.github.com>
2020-10-04 10:15:47 +0530

    Create Pair sum in array .cpp

---
+++ Programs/Pair sum in array .cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#include "solution.h"
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int size;
+		int x;
+
+		cin >> size;
+		int *input = new int[size];
+
+		for (int i = 0; i < size; i++)
+		{
+			cin >> input[i];
+		}
+
+		cin >> x;
+
+		cout << pairSum(input, size, x) << endl;
+
+		delete[] input;
+	}
+	
+	return 0;
+}
