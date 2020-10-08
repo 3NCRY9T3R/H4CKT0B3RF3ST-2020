09de967c3887dba0ec05d16894cd5bdf7675597f
shivani9-codes <72536686+shivani9-codes@users.noreply.github.com>
2020-10-08 08:59:56 +0530

    Create Stock_Span.java

---
+++ programs/Stock_Span.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void display(int[] a) {
+    StringBuilder sb = new StringBuilder();
+
+    for (int val : a) {
+      sb.append(val + "\n");
+    }
+    System.out.println(sb);
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for (int i = 0; i < n; i++) {
+      a[i] = Integer.parseInt(br.readLine());
+    }
+
+    int[] nge = solve(a);
+    display(nge);
+  }
+
+  public static int[] solve(int[] arr) {
+    int[] span = new int[arr.length];
+
+    Stack<Integer> st = new Stack<>();
+    st.push(0);
+    span[0] = 1;
+
+    for (int i = 1; i < arr.length; i++) {
+      while(st.size() > 0 && arr[i] > arr[st.peek()]){
+        st.pop();
+      }
+
+      if(st.size() == 0){
+        span[i] = i + 1;
+      } else {
+        span[i] = i - st.peek();
+      }
+
+      st.push(i);
+    }
+
+    return span;
+  }
+
+}
