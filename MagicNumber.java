81a0ae01913a8da8aee996d7783dc8e413494227
Ayush Pandey <59275479+Ayush7-BIT@users.noreply.github.com>
2020-10-13 11:52:05 +0530

    Added Code for MagicNumber in Java

---
+++ MagicNumber.java
@@ -0,0 +1,13 @@
+public class Magic {
+  //Driver Method
+  public static void main(String args[])
+  {
+        //cheecking for pre-defined inputs
+        int sum=50113;
+        //Condition check for magic number
+        if(sum%9==1)
+           System.out.println("Magic Number");
+        else
+        System.out.print("Not a Magic Number");
+        }
+      }  
