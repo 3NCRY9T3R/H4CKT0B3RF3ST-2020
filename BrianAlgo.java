a71e9747fc67df5931740cef31b4b9f7f90a2a29
Vinamra Kumar Jain <vinamrakumarjain@gmail.com>
2020-10-31 23:40:11 +0530

    Create BrianAlgo.java
    This question was asked in DXC Technology interview 2020

---
+++ BrianAlgo.java
@@ -0,0 +1,20 @@
+import java.util.*; 
+// Function to get no of set bits in binary representationof positive integer n  [DXC Technology-2020]
+class countSetBits { 
+    public static void main(String args[]) 
+    { 
+        Scanner s = new Scanner(System.in);
+        int i = s.nextInt(); 
+        System.out.println(countSetBits(i)); 
+    }
+    //Brian's Algorithm
+    static int countSetBits(int n) 
+    { 
+        int res = 0; 
+        while (n > 0) { 
+            n = (n & (n-1));
+            res++;
+        } 
+        return res; 
+    }   
+}    
