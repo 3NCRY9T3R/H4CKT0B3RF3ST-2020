7c6aa8b57062dc41ab22b96cce21f68e6dc29e9d
Shreyansh Singh <shreyanshsingh2107@gmail.com>
2020-10-31 22:05:56 +0530

    Delete translator.py

--- translator.py
+++
@@ -1,6 +0,0 @@
-from googletrans import Translator, LANGUAGES
-text=input("Enter a text to translate")
-
-for language in LANGUAGES:
-  t=Translator().translate(text,dest=language)
-  print(LANGUAGES[language]+': '+ t.text)
